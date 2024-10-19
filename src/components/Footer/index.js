8a7736095be4a5bc5f6c50dab7e47c61f380ef3d
tejaswi kurapati <tejaswikurapati88@gmail.com>
2024-10-19 16:09:41 +0530

    updated footer with sub

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,32 @@
 import './index.css'
+import {useState} from 'react'
 
 const Footer =()=>{
+    const [email, setEmail]= useState('')
+
+    const onEmail=(event)=>{
+        setEmail(event.target.value)
+    }
+
     return (
-        <h1>Footer</h1>
+        <div className='footer-bg-cont'>
+            <div className='footer-cont1'>
+                <div>
+                    <h1 className='sub-heading'>SUBSCRIBE TO OUR NEWSLETTER</h1>
+                    <p className='para-footer'>Lorem ipsum dolor sit amet. 
+                        Aut ipsam illum et nostrum quidem aut necesbus enim
+                    </p>
+                    <input 
+                        type='text' 
+                        value={email} 
+                        className='email-inp' 
+                        onChange={onEmail} 
+                        placeholder='Enter Your Email Address' 
+                    />
+                    <button className='subs-btn'>SUBSCRIBE</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
